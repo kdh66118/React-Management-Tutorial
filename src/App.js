3adfbd685a1c12dc3d2508c021572bd9751c472c
kdh66118 <kdh66118@naver.com>
2020-12-17 01:39:57 +0900

    origin https://github.com/kdh66118/test02_web.git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Progress } from 'semantic-ui-react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>안녕하세요zz</p>
+      <Button primary>하이하이</Button>
+      <Progress percent = {76}/>
+
     </div>
   );
 }
